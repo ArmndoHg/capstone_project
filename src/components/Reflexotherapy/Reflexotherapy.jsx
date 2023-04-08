bae66fe27f4af9860de68c04c35c2d46ef7f4a98
ArmndoHg <armndohernndez@gmail.com>
2023-04-09 01:02:06 +0200

    styling

--- src/components/Reflexotherapy/Reflexotherapy.jsx
+++ src/components/Reflexotherapy/Reflexotherapy.jsx
@@ -2,100 +2,90 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "./reflexotherapy.css";
 import Divider from "../Divider/Divider";
-import { TbBrandDrops } from "react-icons/tb";
+import { GiFootprint } from "react-icons/gi";
 
 const Reflexotherapy = () => (
-  <Row>
-    <Col lg={12}>
-      {" "}
-      <Divider header="Reflexotherapy" />
-    </Col>
-    <Col lg={6}>
-      <div className="reflexo-body">
-        <div className="reflexo-section1">
-          <ul className="ul-dots">
-            <li className="list-body d-flex">
-              <span className="list-icon d-flex">
-                <TbBrandDrops size={35} />
-              </span>
-              <span className="list-icon-description">
-                Helps to restore proper health in a natural way.
-              </span>
-            </li>
-            <li className="list-body d-flex">
-              <span className="list-icon d-flex">
-                <TbBrandDrops size={35} />
-              </span>
-              <span className="list-icon-description">
-                Naturopaths use traditional medical systems from around the
-                world.
-              </span>
-            </li>
-            <li className="list-body d-flex">
-              <span className="list-icon d-flex">
-                <TbBrandDrops size={35} />
-              </span>
-              <span className="list-icon-description">
-                <font>
-                  Primarily the composition of natural plant ingredients with
-                  each other in such a way that they show a specific synergistic
-                  healing effect.
-                </font>
-              </span>
-            </li>
-            <li className="list-body d-flex">
-              <span className="list-icon d-flex">
-                <TbBrandDrops size={35} />
-              </span>
-              <span className="list-icon-description">
-                The unique properties of certain plants enable effective action
-                showing analgesic, bactericidal, anti-inflammatory or calming
-                properties.
-              </span>
-            </li>
-            <li className="list-body d-flex">
-              <span className="list-icon d-flex">
-                <TbBrandDrops size={35} />
-              </span>
-              <span className="list-icon-description">
-                Psychological aspect of the patient it is often the key that
-                underlies many diseases and is the cause of many different
-                physical ailments.
-              </span>
-            </li>
-            <li className="list-body d-flex">
-              <span className="list-icon d-flex">
-                <TbBrandDrops size={35} />
-              </span>
-              <span className="list-icon-description">
-                Helps to restore proper health in a natural way.
-              </span>
-            </li>
-          </ul>
+  <Container fluid>
+    <Row className="ml-5 mr-5">
+      <Col lg={12}>
+        {" "}
+        <Divider header="Reflexotherapy" />
+      </Col>
+      <Col lg={6} className="reflexo-descrption">
+        <div className="reflexo-body">
+          <div className="reflexo-section1">
+            <ul className="ul-dots">
+              <li className="list-body d-flex">
+                <span className="list-icon d-flex">
+                  <GiFootprint size={35} style={{ color: "#322d2dd9" }} />
+                </span>
+                <span className="list-icon-description">
+                  Ability to stimulate nerve function.
+                </span>
+              </li>
+              <li className="list-body d-flex">
+                <span className="list-icon d-flex">
+                  <GiFootprint size={35} style={{ color: "#322d2dd9" }} />
+                </span>
+                <span className="list-icon-description">Increase energy.</span>
+              </li>
+              <li className="list-body d-flex">
+                <span className="list-icon d-flex">
+                  <GiFootprint size={35} style={{ color: "#322d2dd9" }} />
+                </span>
+                <span className="list-icon-description">
+                  Boost circulation.
+                </span>
+              </li>
+              <li className="list-body d-flex">
+                <span className="list-icon d-flex">
+                  <GiFootprint size={35} style={{ color: "#322d2dd9" }} />
+                </span>
+                <span className="list-icon-description">
+                  Induce a deep state of relaxation.
+                </span>
+              </li>
+              <li className="list-body d-flex">
+                <span className="list-icon d-flex">
+                  <GiFootprint size={35} style={{ color: "#322d2dd9" }} />
+                </span>
+                <span className="list-icon-description">Eliminate toxins.</span>
+              </li>
+              <li className="list-body d-flex">
+                <span className="list-icon d-flex">
+                  <GiFootprint size={35} style={{ color: "#322d2dd9" }} />
+                </span>
+                <span className="list-icon-description">
+                  Stimulate the central nervous system.
+                </span>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </Col>
-    <Col lg={6}>
-      <div className="reflexo-body">
-        <div className="reflexo-section1">
-          <img
-            src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-1086849572.jpg?crop=0.668xw:1.00xh;0.282xw,0&resize=480:*"
-            alt=""
-          />
+      </Col>
+      <Col lg={6}>
+        <div className="reflexo-body">
+          <div className="  reflexo-section2">
+            <img
+              src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-1086849572.jpg?crop=0.668xw:1.00xh;0.282xw,0&resize=480:*"
+              alt=""
+              className="reflexo-photo"
+            />
+          </div>
         </div>
-      </div>
-    </Col>
-    <Col lg={6}>
-      <div className="reflexo-body">
-        <div className="reflexo-section1"></div>
-      </div>
-    </Col>
-    <Col lg={6}>
-      <div className="reflexo-body">
-        <div className="reflexo-section1"></div>
-      </div>
-    </Col>
-  </Row>
+      </Col>
+      <Col lg={6}>
+        <div className="reflexo-body">
+          <div className="reflexo-section3"></div>
+        </div>
+      </Col>
+      <Col lg={6}>
+        <div className="reflexo-body">
+          <div className="reflexo-section4"></div>
+        </div>
+      </Col>
+    </Row>
+  </Container>
 );
 
 export default Reflexotherapy;
